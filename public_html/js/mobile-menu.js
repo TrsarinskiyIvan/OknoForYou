e6e874fdc6afb604bfa0def7109e350b8d780ffc
Администратор <@BEST-23EQO3KREH>
2017-07-23 19:10:49 +0300

    snapshot 6

---
+++ public_html/js/mobile-menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    
+    $(".buttonMenu").click(function (){
+        $(".mobile-side-bar").slideDown();
+    });
+    
+    $(".mobile-side-bar").click(function (){
+        $(".mobile-side-bar").slideUp();
+    });
+    
+    
+});
+
