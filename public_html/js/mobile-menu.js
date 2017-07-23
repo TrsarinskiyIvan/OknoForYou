916ad44d91dc1ed6e9570414bcf99088446d8633
Администратор <@BEST-23EQO3KREH>
2017-07-23 19:32:55 +0300

    snapshot 7

--- public_html/js/mobile-menu.js
+++ public_html/js/mobile-menu.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
 
-    
-    $(".buttonMenu").click(function (){
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 640) {
+            $(".mobile-side-bar").show();
+        }
+    });
+
+    window.addEventListener("scroll", function () {
+        if (window.innerWidth < 640) {
+            $(".mobile-side-bar").slideUp();
+        }
+
+    });
+
+    $(".buttonMenu").click(function () {
         $(".mobile-side-bar").slideDown();
     });
-    
-    $(".mobile-side-bar").click(function (){
-        $(".mobile-side-bar").slideUp();
-    });
-    
-    
+
 });
 
